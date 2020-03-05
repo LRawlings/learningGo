bf164f2645096cb81de8f7c87dcc94eedf2724b8
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-05 18:23:44 +0000

    Added test for hello 'name'

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Luke")
+	want := "Hello, Luke"
 
 	if got != want {
 		t.Errorf("Got %q want %q", got, want)
