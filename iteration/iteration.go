3214fb1b72e44743b6b9cc32b6ca25e78e6993b3
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-07 13:44:58 +0000

    added int for number of times repeated

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, numberRepeated int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < numberRepeated; i++ {
 		repeated += character
 	}
 	return repeated
