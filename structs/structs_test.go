e5ecf8c607599186a7b99dd3a1da108f75805fc8
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-08 09:54:14 +0000

    intro to interfaces

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -16,24 +16,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %f", got, want)
+		}
+	}
+
 	t.Run("area of rectangle", func(t *testing.T) {
 		rectangle := Rectangle{19.0, 5.0}
-		got := rectangle.Area()
-		want := 95.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, 95.0)
 	})
 
 	t.Run("area of circle", func(t *testing.T) {
 		circle := Circle{15}
-		got := circle.Area()
-		want := 47.12388980384689
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 47.12388980384689)
 	})
 
 }
