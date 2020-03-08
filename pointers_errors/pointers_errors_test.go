4ac6f9ec04c3e3a15a1facfbf962664725d092a1
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-08 12:31:52 +0000

    refactored tests to clean up assertions

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -6,33 +6,22 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assterBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-
-		//fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
-		want := Bitcoin(10)
-
 		if got != want {
 			t.Errorf("got %s got %s", got, want)
 		}
+	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assterBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
-
 		wallet.Withdraw(Bitcoin(6))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(4)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assterBalance(t, wallet, Bitcoin(4))
 	})
 }
