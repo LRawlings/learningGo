b1afb3c548e4994c738b9c2aa4d6d8b2e54b9b11
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-08 09:47:10 +0000

    intro to methods

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -2,27 +2,38 @@ package main
 
 import "testing"
 
-type Rectangle struct {
-	width  float64
-	height float64
-}
-
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("peremeter of rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{19.0, 5.0}
-	got := Area(rectangle)
-	want := 95.0
+	t.Run("area of rectangle", func(t *testing.T) {
+		rectangle := Rectangle{19.0, 5.0}
+		got := rectangle.Area()
+		want := 95.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("area of circle", func(t *testing.T) {
+		circle := Circle{15}
+		got := circle.Area()
+		want := 47.12388980384689
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
 }
