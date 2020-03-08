ad775a107831811188739cd71ea6d49f1c4948c5
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-08 12:22:05 +0000

    add withraw method

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -6,17 +6,33 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+		got := wallet.Balance()
 
-	//fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		//fmt.Printf("address of balance in test is %v \n", &wallet.balance)
 
-	want := Bitcoin(10)
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s got %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s got %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+
+		wallet.Withdraw(Bitcoin(6))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(4)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
