b52af5fd5532e721bec7f645f185e2dcbf132ab9
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-08 16:25:17 +0000

    add test for missing key

--- dictionary/dicionary_test.go
+++ dictionary/dicionary_test.go
@@ -3,17 +3,33 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	dictionary := Dictionary{"test":"this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("search for word that exist in dictionary", func(t *testing.T){
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+	
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(t, got, want)
+	t.Run("search for word that doesn't exist in dictionary", func(t *testing.T){
+		_, got := dictionary.Search("rum")
+		assertError(t, got, ErrNotFound)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,9 +1,17 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+var ErrNotFound = errors.New("this word is not in the dictionary")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
 
 func Search(dictionary map[string]string, word string) string {
