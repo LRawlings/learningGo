09f3671d5d2f361c550c3821f2d6bd938048db1f
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-08 09:20:31 +0000

    refactored to use struct

--- structs/structs.go
+++ structs/structs.go
@@ -1,9 +1,9 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -2,8 +2,14 @@ package main
 
 import "testing"
 
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,7 +18,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(19.0, 5.0)
+	rectangle := Rectangle{19.0, 5.0}
+	got := Area(rectangle)
 	want := 95.0
 
 	if got != want {
