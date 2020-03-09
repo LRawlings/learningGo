48b742b4ec8d7947f31794539a307bd80edaccb7
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-09 19:34:32 +0000

    swap default time (nanoseconds?) for time.Second

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -10,11 +10,12 @@ import (
 func Countdown(out io.Writer) {
 	const finalWord = "Go!"
 	const countdownStart = 3
-	const delay = 1000000000
+	const delay = 1
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(delay)
+		time.Sleep(delay * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(delay * time.Second)
 	fmt.Fprint(out, finalWord)
 }
 
