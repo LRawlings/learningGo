f8e8eb1410af0703b7b34b18d5f527ffaffbfc54
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-09 19:32:58 +0000

    added delay (into to time.Seep)

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func Countdown(out io.Writer) {
 	const finalWord = "Go!"
 	const countdownStart = 3
+	const delay = 1000000000
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(delay)
 		fmt.Fprintln(out, i)
 	}
 	fmt.Fprint(out, finalWord)
