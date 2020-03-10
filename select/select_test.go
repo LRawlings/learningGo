c3cba37afe26e18d982c6dfebedbc5048092a28f
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-10 18:58:36 +0000

    change test to use mock http server

--- select/select_test.go
+++ select/select_test.go
@@ -1,11 +1,26 @@
 package main
 
-import ("testing")
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
 
-	slowURL := "http://tpjb.co.uk"
-	fastURL := "http://www.quii.co.uk"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(19 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -13,4 +28,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
