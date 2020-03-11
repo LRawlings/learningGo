0342b1f84afc2e8091687918b120f57b7cc63f5f
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-11 15:33:58 +0000

    initial commit

---
+++ sync/sync_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T){
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCount(t, counter, 3)
+	})
+}
+
+func assertCount(t *testing.T, got Counter, want int) {
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
