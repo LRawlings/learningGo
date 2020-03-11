db8c2ad1af32ea7579cb9ae624af95b90ca30eee
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-11 13:51:38 +0000

    add test for more than one string field

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -19,6 +19,14 @@ func TestWalk(t *testing.T) {
 			}{"Chris"},
 			[]string{"Chris"},
 		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name  string
+				Email string
+			}{"John", "john@email.com"},
+			[]string{"John", "john@email.com"},
+		},
 	}
 
 	for _, test := range cases {
