e8b108c5926456967fc62322e020265a9a8b2e68
Luke Rawlings <35060457+LRawlings@users.noreply.github.com>
2020-03-12 19:28:10 +0000

    added up to 39

--- roman_numerals/roman_numerals_test.go
+++ roman_numerals/roman_numerals_test.go
@@ -19,6 +19,12 @@ func TestRomanNumerals(t *testing.T) {
 		{"8 gets converted to VIII", 8, "VIII"},
 		{"9 gets converted to IX", 9, "IX"},
 		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"19 gets converted to XIX", 19, "XIX"},
+		{"23 gets converted to XXIII", 23, "XXIII"},
+		{"26 gets converted to XXVI", 26, "XXVI"},
+		{"35 gets converted to XXXV", 35, "XXXV"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
